d3670c4fb7da6082f7234a0c1e4db549ad3074eb
Mr.Dust <56252828+MrDust0@users.noreply.github.com>
2022-04-26 14:59:16 +0800

    Create dynamic_property.py

---
+++ dynamic_property.py
@@ -0,0 +1,31 @@
+class FakeProperty:
+
+    def __init__(self, attr: str, dtype:Type) -> None:
+        self.attr = attr
+        self.dtype = dtype
+
+    def __get__(self, obj, objtype):
+        return obj._cache[self.attr]
+
+    def __set__(self, obj, value):
+        if not isinstance(value, self.dtype):
+            raise TypeError("Attribute: %s requires tpye: %s, but type:%s is gotten.",self.attr, self.dtype, type(value))
+        obj._cache[self.attr] = value
+    
+    def __delete__(self, obj):
+        obj._cache[self.attr] = 0
+
+def dynamic_property(*args: Any, **kwargs: Any):
+    def decorate(cls):
+        for attr, dtype in kwargs.items():
+            setattr(cls, attr, FakeProperty(attr, dtype))
+        return cls
+    return decorate
+
+  
+@dynamic_property(
+    slot1 = int,
+    slot2 = str
+)
+class Test():
+    pass
