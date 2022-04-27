f8c2619aa981f5ae59bf1e7a0e33ead10efbcef5
Mr.Dust <56252828+MrDust0@users.noreply.github.com>
2022-04-27 10:42:39 +0800

    Update singleton.py

--- singleton.py
+++ singleton.py
@@ -1,8 +1,11 @@
+# https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
 class Singleton(type):
     _instances = {}
     def __call__(cls, *args: Any, **kwds: Any) -> Any:
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwds)
+        else:
+            cls._instances[cls].__init__(*args, **kwargs)
         return cls._instances[cls]
 
 class Test(metaclass=Singleton):
